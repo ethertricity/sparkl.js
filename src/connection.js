707bdaa804e853db7fa97feb634ccd1c93eaace6
euvl <yev.vlasenko@gmail.com>
2016-10-12 14:26:02 +0100

    Removed old files

--- src/connection.js
+++ src/connection.js
@@ -1,4 +1,6 @@
-/***************************************************************************
+/**
+ * Copyright (c) 2015 SPARKL Limited. All Rights Reserved.
+ * Author <jacoby@sparkl.com> Jacoby Thwaites.
  *
  * Connection object constructor.
  *
@@ -8,222 +10,313 @@
  * where the service ID or pathname is appended to the websocket URL. That's
  * important because execute permission on the referenced service is
  * required to open the websocket.
- *
- ***************************************************************************/
-
-var Connection = function Connection(url) {
-  this.url = url;
-  this.pendingReplies = {}; // Pending replies keyed by id.
-  this.pendingResponses = {}; // Pending Sparkl responses keyed by id
-  this.nextRequestId = counter();
-  this.nextSolicitId = counter();
-  this.ws = null; // Websocket to SPARKL router.
-  this.serviceInstances = {}; // Service instances keyed by instance ID.
-  this.eventHandler = // Our event handler.
-    new EventHandler(this, ["preopen", "open"]);
-
-  this.eventHandler.delegate();
-};
-
-Connection.READYSTATE_CONNECTING = 0;
-Connection.READYSTATE_OPEN = 1;
-Connection.READYSTATE_CLOSING = 2;
-Connection.READYSTATE_CLOSED = 3;
-
-// The connection is open if the websocket is open and the open
-// for the connection itself has been received.
-Connection.prototype.isWebSocketOpen = function () {
-  var connection = this;
-  return connection.ws && (connection.ws.readyState === Connection.READYSTATE_OPEN);
-};
-
-// The web socket is opened upon first service open.
-Connection.prototype.openWebSocket = function (serviceId) {
-  var connection = this;
-
-  return new Promise(function (fulfil, reject) {
-    console.log("Opening: " + connection.url);
-
-    connection.ws = new WebSocket(connection.url + "/" + serviceId);
-
-    connection.ws.onopen = function (event) {
-      console.log("Websocket opened");
-      fulfil();
-    };
-
-    connection.ws.onclose = function (event) {
-      console.log("Websocket closed");
-      connection.onClose();
-    };
-
-    connection.ws.onmessage = function (message) {
-      connection.onMessage(message);
-    };
-
-    connection.ws.onerror = function (event) {
-      reason = "Failed to open websocket";
-      console.error(reason);
-      reject(reason);
-    };
-  });
-};
-
-/**
- * Opens the service. If the base URL is provided, the service Javascript
- * source href is resolved against it.
- *
- * If the base URL is a complete pathname, and there is no source property
- * in the service definition, then the base alone is the source for the
- * implementation Javascript.
- *
- * @param serviceId the service Id or pathname.
  */
-Connection.prototype.open = function (serviceId) {
-  var connection = this;
 
-  return new Promise(function (fulfil, reject) {
+(function(global) {
+  var sparkl = global.sparkl;
 
-    // If the websocket isn't open, open it.
-    // The open event will follow.
-    if (!connection.isWebSocketOpen()) {
-      connection.openWebSocket(serviceId).then(
-        function () {
-          fulfil();
-        },
+  var Connection = function Connection(url) {
+    this.url = url;
+    this.ws = null;             // Websocket to SPARKL.
+    this.serviceInstances = {}; // Service instances keyed by instance id.
+    this.eventHandler =         // Our event handler.
+      new sparkl.EventHandler(this, [
+        "open", "close", "service_preopen", "service_open", "service_close"]);
 
-        function (error) {
-          reason = "Service: " + serviceId + ", error: " + error;
-          console.error(reason);
-          reject(reason);
-        });
-    }
 
-    // If the websocket is open, send an open request.
-    // The open event will follow.
-    else {
-      connection.sendMessageAwaitAck({
-        "tag": "open",
-        "serviceId": serviceId
-      })
-        .then(
-        function () {
-          fulfil();
-        },
+    // Each pending response is keyed by its ref and looks like this:
+    // {
+    //   ref: data_event_ref,
+    //   fulfil: fulfil_fun,
+    //   reject: reject_fun,
+    //   timeout: timeout_reference
+    // }
+    this.pendingResponses = {};
 
-        function (reply) {
-          console.error(reply.reason());
-          reject("Rejected: " + reply.reason());
-        });
-    }
-  });
-};
+    this.eventHandler.delegate();
+  };
 
-Connection.prototype.onClose = function () {
-  var connection = this;
+  Connection.READYSTATE_CONNECTING = 0;
+  Connection.READYSTATE_OPEN = 1;
+  Connection.READYSTATE_CLOSING = 2;
+  Connection.READYSTATE_CLOSED = 3;
 
-  foreach(connection.serviceInstances,
-    function (serviceInstance) {
-      serviceInstance.onClose();
-    });
+  // The connection is open if the websocket is open.
+  Connection.prototype.isWebSocketOpen = function() {
+    var connection = this;
+    return connection.ws
+      && (connection.ws.readyState === Connection.READYSTATE_OPEN);
+  };
 
-  connection.fireEvent("close");
-  console.log("Closed all service instances");
-};
-
-Connection.prototype.sendMessage = function (message) {
-  var connection = this;
-  connection.ws.send(JSON.stringify(message));
-};
-
-Connection.prototype.sendMessageAwaitAck =
-  function (message, timeout) {
+  /**
+   * Returns a promise which is fulfilled when the websocket has
+   * opened successfully, otherwise it is rejected.
+   */
+  Connection.prototype.openWebSocket = function() {
     var connection = this;
 
-    return new Promise(function (fulfil, reject) {
-      var messageId = "req" + connection.nextRequestId();
-      var timeoutMillis = timeout || 5000; // Default 5s message timeout.
-      var timeout = setTimeout(
-        function () {
-          reason = "Timeout" + messageId;
-          delete connection.pendingReplies[messageId];
-          console.error(reason);
-          reject(reason);
-        }, timeoutMillis);
-      var pendingReply = {
-        msgid: messageId,
-        fulfil: fulfil,
-        reject: reject,
-        timeout: timeout
+    return new Promise(function(fulfil, reject) {
+      console.log("Opening: " + connection.url);
+
+      connection.ws = new WebSocket(connection.url);
+
+      connection.ws.onopen = function(event) {
+        console.log("Websocket opened");
+        fulfil();
       };
 
-      connection.pendingReplies[messageId] = pendingReply;
-      message[MSG_ID] = messageId;
-      connection.ws.send(JSON.stringify(message));
+      connection.ws.onclose = function(event) {
+        connection.onClose();
+        console.log("Websocket closed");
+      };
+
+      connection.ws.onmessage = function(message) {
+        connection.onMessage(message);
+      };
+
+      connection.ws.onerror = function(event) {
+        reject(event);
+      };
     });
   };
 
-Connection.prototype.onMessage = function (rawMessage) {
-  var connection = this;
-  var message = new sparkl.Struct(rawMessage.data); // Message is wrapped.
-  var msgId = message.attr(MSG_ID);
-  var pendingReply = msgId ?
-    connection.pendingReplies[msgId] :
-    false;
+  /**
+   * Opens the service. If the base URL is provided, the service Javascript
+   * source href is resolved against it.
+   *
+   * If the base URL is a complete pathname, and there is no source property
+   * in the service definition, then the base alone is the source for the
+   * implementation Javascript.
+   *
+   * @param serviceId the service Id or pathname.
+   */
+  Connection.prototype.open = function() {
+    var connection = this;
 
-  if (pendingReply) {
-    delete connection.pendingReplies[message._msgid()];
-    clearTimeout(pendingReply.timeout);
+    return new Promise(function(fulfil, reject) {
 
-    if (message.tag() === "error") {
-      pendingReply.reject(message);
-    } else if (message.tag() === "ok") { // Dependent service open-event.
-      pendingReply.fulfil(message);
-    }
-  }
+      // If the websocket isn't open, open it.
+      // The open event will follow.
+      if (!connection.isWebSocketOpen()) {
+        connection.openWebSocket().then(
+          function() {
+            connection.fireEvent("open", connection);
+            fulfil();
+          },
 
-  // The connection open-event is sent asynchronously by SPARKL.
-  else if (message.tag() === "open-event") {
-    connection.createNewServiceInstance(message);
-  }
+          function(error) {
+            console.error(error);
+            reject(error);
+          });
+      }
 
-  // All other messages are aimed at an opened service instance.
-  else {
-    var serviceInstance =
-      connection.serviceInstances[message.instanceId()];
-
-    if (serviceInstance) {
-      serviceInstance.enqueue(message);
-    } else {
-      reason = "Invalid instance ID: " + message.instanceId();
-      console.error(reason);
-    }
-  }
-};
-
-/**
- * Creates a new service instance.
- *
- * Two events are fired:
- *   preopen - allows actions such as setting Javascript base URL.
- *   open    - fired upon successful load and initialisation.
- *
- * @param openEvent the open event received from SPARKL.
- */
-Connection.prototype.createNewServiceInstance = function (openEvent) {
-  var connection = this;
-  var instanceId = openEvent.instanceId();
-  var serviceInstance = new ServiceInstance(connection, openEvent);
-
-  console.log("Opening instance: " + instanceId);
-  connection.serviceInstances[instanceId] = serviceInstance;
-  connection.fireEvent("preopen", serviceInstance);
-
-  serviceInstance.open().then(
-    function () {
-      connection.fireEvent("open", serviceInstance);
-    },
-
-    function (error) {
-      console.error("Failed to open service instance: " + error);
+      // If the websocket is open, send an open request.
+      // The open event will follow.
+      else {
+       reject("Websocket already open");
+      }
     });
-};
+  };
+
+  /**
+   * Closes the connection by closing the websocket. This will fire
+   * the onclose events set up in the openWebSocket method.
+   *
+   * This method does nothing if the connection is already closed.
+   */
+  Connection.prototype.close = function() {
+    var connection = this;
+    connection.ws.close();
+  };
+
+  /**
+   * When the connection is closed, all service instances are also
+   * closed.
+   */
+  Connection.prototype.onClose = function() {
+    var connection = this;
+
+    sparkl.util.foldl(
+      function(serviceId, serviceInstance) {
+        connection.closeService(serviceInstance);
+      }, null, connection.serviceInstances);
+
+    connection.fireEvent("close", connection);
+    console.log("Closed all service instances");
+  };
+
+  /**
+   * Sends the message as a JSON encoded string.
+   */
+  Connection.prototype.sendMessage = function(message) {
+    var connection = this;
+    connection.ws.send(JSON.stringify(message));
+  };
+
+  /**
+   * All received messages correspond to events per the tag as follows:
+   * - open_event:  opens a new service instance with metadata.
+   * - close_event: closes the referenced service instance.
+   * - data_event:  contains request, consume or response data.
+   * - error_event: this references either a pending solicit or an instance.
+   */
+  Connection.prototype.onMessage = function(rawMessage) {
+    var connection =
+      this;
+    var message =
+      new sparkl.Struct(rawMessage.data); // Message is wrapped.
+    var tag =
+      message.tag();
+    var
+      id,
+      ref,
+      pendingResponse,
+      responseId,
+      serviceInstance,
+      dataEvent,
+      subject;
+
+    if (tag === "open_event") {
+      connection.openService(message);
+    }
+
+    else if (tag === "close_event") {
+      ref = message.ref();
+      serviceInstance =
+        connection.serviceInstances[ref];
+      connection.closeService(serviceInstance, message);
+    }
+
+    else if (tag === "data_event" || tag === "error_event") {
+      ref = message.ref();
+      pendingResponse =
+        connection.pendingResponses[ref];
+
+      if (pendingResponse) {
+        delete connection.pendingResponses[ref];
+        clearTimeout(pendingResponse.timeout);
+
+        if (tag === "data_event") {
+          responseId =
+            message.attr("subject");
+
+          serviceInstance =
+            pendingResponse.instance;
+
+          subject =
+            serviceInstance.openEvent
+              .content("responses")[0]
+              .content("response", responseId);
+
+          dataEvent =
+            new sparkl.DataEvent(serviceInstance, message)
+              .setSubject(subject);
+
+          pendingResponse.fulfil(dataEvent);
+        }
+        else {
+          pendingResponse.reject(message);
+        }
+      }
+
+      // Message references an instance.
+      else {
+        serviceInstance =
+          connection.serviceInstances[ref];
+
+        if (serviceInstance) {
+          serviceInstance.enqueue(message);
+        }
+        else {
+          console.error(message);
+        }
+      }
+    }
+  };
+
+  /**
+   * Adds a pending response to our table, keyed by reference.
+   */
+  Connection.prototype.addPendingResponse = function(pending) {
+    var connection =
+      this;
+
+    connection.pendingResponses[pending.ref] =
+      pending;
+  };
+
+  /**
+   * Creates a new service instance.
+   *
+   * The openEvent contains all the metadata about the service including
+   * the operations and fields.
+   *
+   * Two events are fired:
+   *   preopen - allows actions such as setting Javascript base URL.
+   *   open    - fired upon successful load and initialisation.
+   *
+   * @param openEvent the open event received from SPARKL.
+   */
+  Connection.prototype.openService = function(openEvent) {
+    var connection =
+      this;
+    var instance =
+      openEvent.ref();
+    var serviceInstance =
+      new sparkl.ServiceInstance(connection, openEvent);
+
+    // By default we resolve non-root hrefs against the folder containing
+    // the service. This resolver can be replaced by the user.
+    var defaultResolver =
+      function(href) {
+        var sourceHref, servicePathname, anchor;
+
+        if (href.startsWith("/")) {
+          return href;
+        }
+        else {
+          sourceHref =
+            "/sse_cfg/source/";
+          servicePathname =
+            openEvent.pathname();
+          anchor =
+            document.createElement("a");
+          anchor.href =
+            sourceHref + servicePathname + "/../" + href;
+          return anchor.href;
+        }
+      };
+
+    serviceInstance.setResolver(defaultResolver);
+
+    console.log("Opening instance: " + instance);
+    connection.serviceInstances[instance] = serviceInstance;
+    connection.fireEvent("service_preopen", serviceInstance);
+
+    serviceInstance
+      .open()
+      .then(
+        function() {
+          connection.fireEvent("service_open", serviceInstance);
+        },
+
+        function(error) {
+          console.error("Failed to open service instance: " + error);
+        });
+  };
+
+  /**
+   * Closes a service instance and fires the appropriate events.
+   */
+  Connection.prototype.closeService = function(serviceInstance, message) {
+    var connection = this;
+    var instanceId = serviceInstance.openEvent.ref();
+    console.log("Closing: " + instanceId);
+    serviceInstance.onClose(message);
+    delete connection.serviceInstances[instanceId];
+    connection.fireEvent("service_close", serviceInstance);
+  };
+
+  sparkl.Connection = Connection;
+
+})(this);
--- src/response.js
+++
@@ -1,8 +0,0 @@
-/*
- * Response is a 'super-class' of AbstractInput.
- */
-var Response = function (responseEvent, serviceInstance) {
-  AbstractInput.call(this, responseEvent, serviceInstance);
-};
-
-Response.prototype = new AbstractInput();
